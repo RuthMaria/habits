f54fe942764581a582375dec7f161e9cd2e06831
RuthMaria <ruth-15rms@hotmail.com>
2023-01-22 22:43:23 -0300

    feat: finish project

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,14 @@
 import './styles/global.css';
+import './lib/dayjs';
 import { Header } from './components/Header';
-// import { SummaryTable } from './components/SummaryTable';
+import { SummaryTable } from './components/SummaryTable';
 
 export const App = () => {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
       <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
         <Header />
-        {/* <SummaryTable /> */}
+        <SummaryTable />
       </div>
     </div>
   );
